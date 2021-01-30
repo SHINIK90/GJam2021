0770dc66c62293cab13283ccd951a79484e127d3
Maurixor <Maurixor@users.noreply.github.com>
2021-01-30 12:18:05 -0500

    New update
    Menu principal con musica, sonido y funciones

---
+++ GJam2021/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource click;
+    public GameObject ControlsPanel;
+    public string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlsPanel.SetActive(false);
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onClickPlay() {
+        click.Play();
+        SceneManager.LoadScene(SceneName);
+    }
+    public void onClickControls() {
+        click.Play();
+        ControlsPanel.SetActive(true);
+    }
+    public void onClickExit() {
+        click.Play();
+    }
+    public void onClickExitControls() {
+        click.Play();
+        ControlsPanel.SetActive(false);
+    }
+}
