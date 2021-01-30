cc43d0b28ea00d96047d13e9ecfd5f70b09f3ffc
Maurixor <Maurixor@users.noreply.github.com>
2021-01-30 10:35:50 -0500

    Barra de vida funcional, HUD

---
+++ GJam2021/Assets/_Scripts/HealthSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public Image HealthBar;
+    public float CurrentHealth;
+    public float MaxHealth;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBar.fillAmount = CurrentHealth / MaxHealth;
+        if (Input.GetKeyDown(KeyCode.A)) {
+            RecibirDaño(10);
+        }
+        if (CurrentHealth >= MaxHealth) {
+            CurrentHealth = MaxHealth;
+        }
+        if (CurrentHealth <= 0) {
+            //dead
+        }
+    }
+    void RecibirDaño(float Damage) {
+        CurrentHealth -= Damage;
+    }
+}
