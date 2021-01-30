9c132dd6a867adcd56e113eb3003bd228a21cdcf
SHINIK90 <muzemail8d@gmail.com>
2021-01-30 14:52:57 -0500

    Character jump

--- GJam2021/Assets/_Scripts/CharController.cs
+++ GJam2021/Assets/_Scripts/CharController.cs
@@ -14,14 +14,25 @@ public class CharController : MonoBehaviour
 
     public float movementSpeed = 20f;
     public Transform body;
+    public Rigidbody rigidbody;
+    public float jumpForce = 10f;
     // Start is called before the first frame update
     void Start(){
         body = transform.Find("Body");
+        rigidbody = transform.GetComponent<Rigidbody>();
+        jumpForce = 10f;
+        movementSpeed = 5f;
     }
 
     // Update is called once per frame
+    void FixedUpdate(){
+        if (Input.GetKey(KeyCode.W) && transform.position == new Vector3(transform.position.x,4,transform.position.z)){
+            rigidbody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        }
+    }
     void Update()
     {
+        //SISTEMA DE MOVIMIENTO
         if (Input.GetKey(KeyCode.A)){
             body.localScale = new Vector3(-1f,body.localScale.y, body.localScale.z);
             transform.position = transform.position + new Vector3(-1f * movementSpeed * Time.deltaTime, 0f, 0f);
@@ -33,6 +44,7 @@ public class CharController : MonoBehaviour
             transform.position = transform.position + new Vector3(1f * movementSpeed * Time.deltaTime, 0f, 0f);
             facingRight = true;
         }
+        
         //SISTEMA DE DISPARO
         if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
