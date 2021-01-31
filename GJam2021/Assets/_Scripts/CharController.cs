48d6f861308c72ea16338bd4a7e288d2a47491e9
SHINIK90 <muzemail8d@gmail.com>
2021-01-30 20:09:15 -0500

    dash

--- GJam2021/Assets/_Scripts/CharController.cs
+++ GJam2021/Assets/_Scripts/CharController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class CharController : MonoBehaviour
 {
-    bool facingRight=true;
+    bool facingRight= true;
+    float facingRightValue= 1f;
     public GameObject derecha, izquierda;
     Vector2 bullet;
     public float fireRate;
@@ -23,6 +24,7 @@ public class CharController : MonoBehaviour
         body = transform.Find("Body");
         rigidbody = transform.GetComponent<Rigidbody>();
         jumpForce = 10f;
+        dashForce = 15f;
         time = dashCooldown;
     }
 
@@ -31,12 +33,21 @@ public class CharController : MonoBehaviour
         if (Input.GetKey(KeyCode.W) && transform.position == new Vector3(transform.position.x,4,transform.position.z)){
             rigidbody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
         }
-    }
-    void Update()
-    {
+        //DASH
         //Debug.Log(time);
         DashCooldownBar.fillAmount = time / dashCooldown;
         time += Time.deltaTime;
+        if(facingRight){facingRightValue = 1f;}else{facingRightValue = -1f;}
+
+        if (Input.GetKey(KeyCode.Space) && time >= dashCooldown) {
+            Debug.Log("DASH");
+            rigidbody.AddForce(dashForce*facingRightValue, 0, 0, ForceMode.Impulse);
+            time = 0;  
+        }
+    }
+    void Update()
+    {
+        
       
         //SISTEMA DE MOVIMIENTO
         if (Input.GetKey(KeyCode.A)){
@@ -50,27 +61,7 @@ public class CharController : MonoBehaviour
             transform.position = transform.position + new Vector3(1f * movementSpeed * Time.deltaTime, 0f, 0f);
             facingRight = true;
         }
-        //DASH
-        if (Input.GetKey(KeyCode.Space)) {
-            if (facingRight)
-            {
-                if (time >= dashCooldown)
-                {
-                    Debug.Log("DASH");
-                    rigidbody.AddForce(dashForce, 0, 0, ForceMode.Impulse);
-                    time = 0;
-                }
-            }
-            else
-            {
-                if (time >= dashCooldown)
-                {
-                    Debug.Log("DASH");
-                    rigidbody.AddForce(-dashForce, 0, 0, ForceMode.Impulse);
-                    time = 0;
-                }
-            }
-        }
+        
         //SISTEMA DE DISPARO
         if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time > nextFire) {
             rigidbody.AddForce(-dashForce, 0, 0, ForceMode.Impulse);
