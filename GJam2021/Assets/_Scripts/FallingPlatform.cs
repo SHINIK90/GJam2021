25d5f9f04577b04183ad40da6e0e88c3cf534977
Maurixor <Maurixor@users.noreply.github.com>
2021-01-31 12:28:41 -0500

    new udpate
    se borro codigo innecesario

--- GJam2021/Assets/_Scripts/FallingPlatform.cs
+++ GJam2021/Assets/_Scripts/FallingPlatform.cs
@@ -9,12 +9,6 @@ public class FallingPlatform : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>(); 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //transform.position = transform.position + new Vector3(0f, -1f * fallingSpeed * Time.deltaTime, 0f);
-    }
     
     private void OnCollisionEnter(Collision collision)
     {
@@ -30,13 +24,4 @@ public class FallingPlatform : MonoBehaviour
         rb.isKinematic = false;
         Destroy(gameObject, 2f);
     }
-    /*
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Invoke("DropPlatform", 0.5f);
-            //Destroy(gameObject, 2f);
-        }
-    }*/
 }
